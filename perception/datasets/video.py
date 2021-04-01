Fix image normalization

perception/datasets/video.py
@@ -24,8 +24,8 @@ def _compute_kernel(size, center):
             kernel[i, j] = _gaussian_kernel(center, x)
     return kernel / kernel.sum()
 
-RGB_MEAN = np.array([0.5, 0.5, 0.5], dtype=np.float32)
-RGB_STD = np.array([0.25, 0.25, 0.25], dtype=np.float32)
+RGB_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+RGB_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
 
 class StereoVideoDataset(IterableDataset):
     LEFT = 0