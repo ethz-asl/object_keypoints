f5d5beb2843a725dbb55894aafba94c0dcef7dfe
Kenneth Blomqvist <kekeblom@gmail.com>
2021-06-30 11:01:15 +0200

    beta = 2

--- perception/loss.py
+++ perception/loss.py
@@ -3,11 +3,10 @@ from torch.nn.modules.loss import _Loss
 from torch.nn import functional as F
 
 class FocalLoss(_Loss):
-    def __init__(self, alpha=2.0, beta=4.0):
+    def __init__(self, alpha=2.0, beta=2.0):
         super().__init__()
         self.alpha = alpha
         self.beta = beta
-        self.eps = torch.tensor(1e-30)
 
     def forward(self, pred, target):
         N = target.shape[0]
