a024092fa9051dcfea55a7219551c386974f5be2
Julian Keller <ethz@juliankeller.net>
2022-01-19 13:16:13 +0100

    Remove ToDos

--- scripts/label_qt.py
+++ scripts/label_qt.py
@@ -366,8 +366,3 @@ if __name__ == '__main__':
     imageViewer.set_current(PATH)
     imageViewer.show()
     sys.exit(app.exec_())
-    # TODO QScrollArea support mouse
-    # base on https://github.com/baoboa/pyqt5/blob/master/examples/widgets/imageviewer.py
-    #
-    # if you need Two Image Synchronous Scrolling in the window by PyQt5 and Python 3
-    # please visit https://gist.github.com/acbetter/e7d0c600fdc0865f4b0ee05a17b858f2
